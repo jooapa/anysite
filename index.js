0d57471392eaf43144659b1e17a6252ee1bdaa3b
jooapas <jooa.akonpelto@gmail.com>
2023-10-23 21:19:13 +0300

    :X

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
-const compression = require("compression");
 const pako = require("pako");
 
 const app = express();
@@ -15,7 +14,6 @@ app.get("/", (req, res) => {
 
 app.post("/makeIt", (req, res) => {
   const htmlData = req.body.html;
-  console.log(htmlData);
   const sanitizedHtml = htmlData.replace(/ /g, "_");
   const compressedHtml = compress(sanitizedHtml); // Compress the HTML data
 
@@ -27,33 +25,21 @@ app.post("/makeIt", (req, res) => {
 
 app.post("/lookUrl", (req, res) => {
   const url = req.body.url;
-  console.log(url);
   const decodedHtml = decodeURIComponent(url);
-  console.log(decodedHtml);
   const decompressedHtml = deCompress(decodedHtml);
-  console.log(decompressedHtml);
   const uriDecodedHtml = decodeURIComponent(decompressedHtml);
-  console.log(uriDecodedHtml);
   const sanitizedHtml = uriDecodedHtml.replace(/_/g, " ");
-  console.log(sanitizedHtml);
   res.json({ url: sanitizedHtml });
 });
 
 app.get("/s", (req, res) => {
   const url = req.query.url;
-  console.log(url);
   const decodedHtml = decodeURIComponent(url);
   const decompressedHtml = deCompress(decodedHtml);
   const uriDecodedHtml = decodeURIComponent(decompressedHtml);
   const sanitizedHtml = uriDecodedHtml.replace(/_/g, " ");
   res.render("anysite", { url: sanitizedHtml });
 });
-
-const compressionOptions = {
-  level: 9,
-  threshold: 0,
-};
-
 
 const compress = (data) => {
   const compressedData = pako.gzip(data);
@@ -66,7 +52,6 @@ const deCompress = (data) => {
   return decompressedData;
 };
 
-app.use(compression(compressionOptions));
 app.use(express.static("public"));
 
 const port = 3000;
