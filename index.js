e10ea2894952b3bc41d618e1408075a47a2123f6
jooapas <jooa.akonpelto@gmail.com>
2023-10-23 18:38:38 +0300

    compress

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const ejs = require("ejs");
-const bodyParser = require("body-parser"); // Add this for parsing JSON data
+const bodyParser = require("body-parser");
+const compression = require("compression");
+const pako = require("pako");
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(bodyParser.json()); // Use bodyParser for JSON parsing
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/site.html");
@@ -13,24 +15,41 @@ app.get("/", (req, res) => {
 
 app.post("/makeIt", (req, res) => {
   const htmlData = req.body.html;
-  // Replace spaces with underscores
   const sanitizedHtml = htmlData.replace(/ /g, "_");
-  const encodedHtml = encodeURIComponent(sanitizedHtml);
+  const compressedHtml = compress(sanitizedHtml); // Compress the HTML data
 
-  // Process the sanitizedHtml and generate a URL as needed
-  const generatedUrl = "http://localhost:3000/s?url=" + encodedHtml;
+  const generatedUrl =
+    "http://localhost:3000/s?url=" + encodeURIComponent(compressedHtml);
 
   res.json({ url: generatedUrl });
 });
 
 app.get("/s", (req, res) => {
-  const encodedHtml = req.query.url;
-  // Decode the URL and replace underscores with spaces
-  const decodedHtml = decodeURIComponent(encodedHtml).replace(/_/g, " ");
-  res.render("anysite", { url: decodedHtml });
+  const url = req.query.url;
+  const decodedHtml = decodeURIComponent(url);
+  const decompressedHtml = deCompress(decodedHtml);
+  const uriDecodedHtml = decodeURIComponent(decompressedHtml);
+  const sanitizedHtml = uriDecodedHtml.replace(/_/g, " ");
+  res.render("anysite", { url: sanitizedHtml });
 });
 
+const compressionOptions = {
+  level: 9,
+  threshold: 0,
+};
 
+app.use(compression(compressionOptions));
+
+const compress = (data) => {
+  const compressedData = pako.gzip(data);
+  return Buffer.from(compressedData).toString("base64");
+};
+
+const deCompress = (data) => {
+  const decodedData = Buffer.from(data, "base64");
+  const decompressedData = pako.ungzip(decodedData, { to: "string" });
+  return decompressedData;
+};
 
 const port = 3000;
 app.listen(port, () => {
